ff16332fabef6f8104928096cd27596e096f1b51
lambda <augusp64@gmail.com>
2021-07-08 15:59:40 -0300

    :art: :computer: Header: Diseño básico & estructura base del proyecto

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "./Componentes/Header/Header";
+import Piano from "./Componentes/Piano/Piano";
+import Footer from "./Componentes/Footer/Footer";
+
+import './normalize.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<Router>
+			<div className="app">
+				<Header />
+				
+				<Piano />
+				
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
