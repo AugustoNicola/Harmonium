565ef5c861f3292dc3ee5093c2a0d7f480ca3e07
lambda <augusp64@gmail.com>
2021-07-08 16:13:07 -0300

    :art: :computer: Footer: Agregado copyright & Siempre pantalla completa

--- app/src/Componentes/Footer/Footer.jsx
+++ app/src/Componentes/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import "./Footer.css";
+
 const Footer = () => {
 	return (
-		<div>
-			
+		<div className="footer">
+			<a target="_blank" rel="noreferrer" href="https://github.com/AugustoNicola/Harmonium"><p>Copyright &copy; 2021 Augusto Nicola <i className="fab fa-github"></i></p></a>
 		</div>
 	)
 }
