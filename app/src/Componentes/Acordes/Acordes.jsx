1ee49be992d963be692b22a2a5a538a97cbfc781
lambda <augusp64@gmail.com>
2021-07-11 20:05:28 -0300

    :wrench: :art: Acordes: Rediseño dinámico con labels y minmax

--- app/src/Componentes/Acordes/Acordes.jsx
+++ app/src/Componentes/Acordes/Acordes.jsx
@@ -6,72 +6,18 @@ import DataAcordes from "../../Data/DataAcordes.json";
 
 const Acordes = () => {
 	return (
-		<div className="acordes">
-			<div className="acorde">
-				<h3>M</h3>
-			</div>
-			<div className="acorde">
-				<h3>m</h3>
-			</div>
-			<div className="acorde">
-				<h3>dim</h3>
-			</div>
-			<div className="acorde">
-				<h3>aug</h3>
-			</div>
-			<div className="acorde">
-				<h3>maj7</h3>
-			</div>
-			<div className="acorde">
-				<h3>7</h3>
-			</div>
-			<div className="acorde">
-				<h3>m(maj7)</h3>
-			</div>
-			<div className="acorde">
-				<h3>m7</h3>
-			</div>
-			<div className="acorde">
-				<h3>maj7&#9839;5</h3>
-			</div>
-			<div className="acorde">
-				<h3>maj7&#x266D;5</h3>
-			</div>
-			<div className="acorde">
-				<h3>7&#9839;5</h3>
-			</div>
-			<div className="acorde">
-				<h3>7&#x266D;5</h3>
-			</div>
-			<div className="acorde">
-				<h3>m7&#x266D;5</h3>
-			</div>
-			<div className="acorde">
-				<h3>dim7</h3>
-			</div>
-			<div className="acorde">
-				<h3>add2</h3>
-			</div>
-			<div className="acorde">
-				<h3>add4</h3>
-			</div>
-			<div className="acorde">
-				<h3>sus2</h3>
-			</div>
-			<div className="acorde">
-				<h3>sus4</h3>
-			</div>
-			<div className="acorde">
-				<h3>6</h3>
-			</div>
-			<div className="acorde">
-				<h3>m6</h3>
-			</div>
-			<div className="acorde">
-				<h3>5</h3>
-			</div>
-			
-		</div>
+		<form id="acordes">
+		{
+			DataAcordes.map((acorde) => {
+				return (
+					<div className="acorde" key={acorde.posicion}>
+						<input type="radio" name="acordeSeleccionado" id={acorde.posicion} value={acorde.posicion} />
+						<label htmlFor={acorde.posicion}>{acorde.nombre}</label>
+					</div>
+				);
+			})
+		}
+		</form>
 	)
 }
 
