717c9093af49fd1da7e8164b05e83eec4f8546cb
lambda <augusp64@gmail.com>
2021-07-15 00:15:01 -0300

    :art: Piano: Estructura y estilos básicos

--- app/src/Componentes/Piano/Piano.jsx
+++ app/src/Componentes/Piano/Piano.jsx
@@ -4,8 +4,59 @@ import "./Piano.css";
 
 const Piano = () => {
 	return (
-		<div className="piano" data-transicion style={{animationDelay: "0s"}}>
-			piano
+		<div className="piano" data-transicion style={{animationDelay: "0.6s"}}>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+				<div className="nota accidental"></div>
+			</div>
+			<div className="grupo">
+				<div className="nota natural"></div>
+			</div>
 		</div>
 	)
 }
